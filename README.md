bf065c10bac0d60fe503cf244b3303ae143a6a6c
Thomas Ackermann <th.acker@arcor.de>
2024-01-03 12:37:39 +0100

    Add first version of code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # mpfritzconn
+
 A minimalistic solution to connect to an AVM FritzBox using MicroPython
+
+FritzBox API:
+  - https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/AHA-HTTP-Interface.pdf
+  - https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/AVM_Technical_Note_-_Session_ID.pdf
+
+As MicroPython lacks proper implementations of an XML parser, MD5 hash generation
+and UTF-16LE string encoding these things are all done by hand in a minimalistic way:
+  - Extracting SID and Challenge from the FritBox's XML response is done by string.replace()/.split()/.tok() with some magic chars
+  - The UTF-16LE encoding of the Challenge Response is done in MicroPython and only works for ASCII strings
+  - The MD5 hash for the Challenge Response is computed using Mauro Rivas MicroPython implementation (Thx!)
+
+Installation:
+  - mip.install("github:tacker66/mpfritzconn")
---
+++ mpfritzconn.py
@@ -0,0 +1,81 @@
+#
+# A minimalistic solution to connect to an AVM FritzBox using MicroPython
+#
+# FritzBox API:
+#   - https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/AHA-HTTP-Interface.pdf
+#   - https://avm.de/fileadmin/user_upload/Global/Service/Schnittstellen/AVM_Technical_Note_-_Session_ID.pdf
+#
+# As MicroPython lacks proper implementations of an XML parser, MD5 hash generation
+# and UTF-16LE string encoding these things are all done by hand in a minimalistic way:
+#   - Extracting SID and Challenge from the FritBox's XML response is done by string.replace()/.split()/.tok() with some magic chars
+#   - The UTF-16LE encoding of the Challenge Response is done in MicroPython and only works for ASCII strings
+#   - The MD5 hash for the Challenge Response is computed using Mauro Rivas MicroPython implementation (Thx!)
+#
+# Installation:
+#   - mip.install("github:tacker66/mpfritzconn")
+#
+
+import md5
+import requests
+
+class MpFritzConn:
+
+    def __init__(self, user, password, url):
+        self.url = url
+        self.sid = self.get_sid(user, password)
+
+    def ascii2utf16le(self, msg):
+        msg = bytearray(msg.encode('utf-8'))
+        buf = bytearray()
+        for c in msg:
+            buf.append(c)
+            buf.append(0)
+        return(buf)
+
+    def get_md5(self, challenge, password):
+        s = self.ascii2utf16le(challenge)
+        s = s + self.ascii2utf16le('-')
+        s = s + self.ascii2utf16le(password)
+        return md5.digest(s)
+        
+    def get_sid(self, user, password):
+        r = requests.get(self.url + '/login_sid.lua')
+        s = r.text
+        s = s.replace("<SID>", "|")
+        s = s.replace("</SID>", "|")
+        sid = s.split("|")[1]
+        s = s.replace("<Challenge>", "€")
+        s = s.replace("</Challenge>", "€")
+        cha = s.split("€")[1]
+        if sid == '0000000000000000':
+            res = cha + '-' + self.get_md5(cha, password)
+            r = requests.get(self.url + '/login_sid.lua?username=' + user + '&response=' + res)
+            s = r.text
+            s = s.replace("<SID>", "|")
+            s = s.replace("</SID>", "|")
+            sid = s.split("|")[1]
+        if sid == '0000000000000000':
+            raise Exception('access denied')
+        return sid
+        
+    def execute(self, cmd, ain=None):
+        if ain:
+            uri = self.url + '/webservices/homeautoswitch.lua?ain=' + ain + '&switchcmd=' + cmd + '&sid=' + self.sid
+        else:
+            uri = self.url + '/webservices/homeautoswitch.lua?switchcmd=' + cmd + '&sid=' + self.sid
+        return requests.get(uri).text
+
+if __name__=='__main__':
+    adr = "http://xx.xx.xx.xx"
+    usr = "<username>"
+    pwd = "<password>"
+    ain = "<DeviceAIN>"
+    fc = MpFritzConn(usr, pwd, adr)
+    ret = fc.execute("getswitchname", ain)
+    print("Name:", ret.strip())
+    ret = fc.execute("getswitchstate", ain)
+    print("State:", ret.strip())
+    ret = fc.execute("getswitchpower", ain)
+    print("Power:", ret.strip())
+    ret = fc.execute("gettemperature", ain)
+    print("Temp:", ret.strip())
